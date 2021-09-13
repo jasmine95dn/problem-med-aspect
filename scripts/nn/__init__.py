30d3ac9effd969b5a2e8e50c8a0a1665bd7f6969
D.H.D. Nguyen <jasmine.1995dn@gmail.com>
2021-09-13 12:41:57 +0200

    update inite no docs

--- scripts/nn/__init__.py
+++ scripts/nn/__init__.py
@@ -1,29 +1,52 @@
 # -*- coding: utf8 -*-
 
 import scripts.nn.processors as processors
-import scripts.nn.config as config
+from scripts.nn.config import EmbeddingConfig, ModelConfig
 from scripts.nn.models import GRUNet, BiLSTMNet, BertNet
 
 
 class ModPolProcessor:
-    def __init__(self, model_config, embedding_config, never_split=None):
-        never_split = ['<p>', '</p>'] if not never_split
-        self.processor = processors.ModelProcessor(model_config=model_config, model=GRUNet,
-                                              embedding_config=embedding_config, never_split=never_split)
+    def __init__(self, model_config:ModelConfig, embedding_config:EmbeddingConfig):
+        """
 
-    def __call__(self, mode='train', info='start', train_model_path=None, save_path=None, infer_save_path=None):
-        self.processor(mode, info, train_model_path, save_path, infer_save_path)
+        :param model_config:
+        :param embedding_config:
+        """
+        self.processor = processors.ModelProcessor(model_config=model_config, model=GRUNet,
+                                                    embedding_config=embedding_config)
+
+    def __call__(self, mode='train', info='start'):
+        """
+
+        :param mode:
+        :param info:
+        :return:
+        """
+        self.processor(mode, info)
+
 
 class RelaProcessor:
-    def __init__(self, model_config, embedding_config, modtype='within'):
+    def __init__(self, model_config, embedding_config, within_mode=True):
+        """
 
-        if modtype == 'within':
+        :param model_config:
+        :param embedding_config:
+        :param within_mode:
+        """
+        if within_mode:
             self.processor = processors.ModelProcessor(model_config=model_config, model=BiLSTMNet,
                                                        embedding_config=embedding_config)
         else:
             self.processor = processors.ModelProcessor(model_config=model_config, model=BertNet,
                                                        embedding_config=embedding_config)
-    def __call__(self, mode='train', info='start', train_model_path=None, save_path=None, infer_save_path=None):
-        self.processor(mode, info, train_model_path, save_path, infer_save_path)
+
+    def __call__(self, mode='train', info='start'):
+        """
+
+        :param mode:
+        :param info:
+        :return:
+        """
+        self.processor(mode, info)
 
 
